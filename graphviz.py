4b917f9c1ff76064a86e56279b29cd66c903b100
takayanagi <tayanagi691@gmail.com>
2024-06-21 11:02:24 +0900

    [update] グラフ描画ができることを確認

--- graphviz.py
+++ graphviz.py
@@ -1,7 +1,10 @@
 import networkx as nx
 import random
 from networkx.drawing.nx_agraph import graphviz_layout
+import pygraphviz as pgv
+
 random.seed(0) # seed値を0に固定
 G = nx.DiGraph([(1,2),(1,3),(2,4),(2,5),(3,6)]) # 1から2，1から3へのエッジとなるような有向グラフの作成
-pos = graphviz_layout(G,prog="dot") # 描画レイアウトの指定 Graphvizの"dot"というレイアウトを使用
-nx.draw(G,pos=pos,with_labels=True) # pos=posでGraphvizのレイアウト通りのグラフを描画
+# g = nx.to_agraph(G)
+g = nx.nx_agraph.to_agraph(G)
+g.draw('test.png',prog='dot')
