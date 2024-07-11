6ab56a58feb16c294b417199c6142d600b6aaa7d
Gerard Glowacki <gerardrglowacki@gmail.com>
2024-07-11 16:54:43 +0900

    Get rid of debug prints

--- visualizer.py
+++ visualizer.py
@@ -5,32 +5,15 @@ import matplotlib.pyplot as plt
 import json
 from quantifier import unify, qs_to_str, level, replace_A8, replace_E8, Quantifier
 
-def classify(qs):
-    clas = ""
-    if qs == []:
-        clas = "C"
-    elif qs[0] == "E" or qs[0] == "A8":
-        clas = "Σ"
-    elif qs[0] == "A" or qs[0] == "E8":
-        clas = "Π"
-    return clas+str(level(qs))
-
-def quantifier_filter(qs,clas):
-    return classify(qs) == clas
-
 if __name__ == "__main__":
     heirarchy = ("S", '3')
     with open("output.json", "r") as f:
         nodes, rels = json.load(f)
     nodes = [Quantifier(node) for node in nodes]
     rels = [(Quantifier(p), Quantifier(q)) for p,q in rels]
-    print([(node, node.classify()) for node in nodes])
-    print(rels)
     Graph = nx.DiGraph()
     for n in nodes:
-        print(n, n.classify())
         if n.classify() == heirarchy:
-            print(n)
             Graph.add_node(str(n.unify()))
 
     for (ps1, ps2) in rels:
